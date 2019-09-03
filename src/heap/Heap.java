50846f07a2096132c5c6858d07cc52988f379385
yasin <357519203@qq.com>
2019-09-03 15:29:06 +0800

    commit

--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -33,7 +33,18 @@ public class Heap {
         heapify(arr, size, 1);
     }
 
-    private void heapify(int[] arr, int size, int i) {
+    private static void buildHeap(int[] arr, int size) {
+        for (int i = size / 2; i >= 1; --i) {
+            heapify(arr, size, i);
+        }
+    }
+
+    /**
+     * @param arr  堆数组
+     * @param size 堆大小
+     * @param i    待放置的元素下标
+     */
+    private static void heapify(int[] arr, int size, int i) {
         while (true) {
             int maxPos = i;
             if (i * 2 <= size && arr[i] < arr[i * 2]) {
@@ -47,11 +58,29 @@ public class Heap {
             }
             swap(arr, maxPos, i);
             i = maxPos;
-
         }
     }
 
-    private void swap(int[] arr, int i1, int i2) {
+    /**
+     * 首先将待排序数组堆化 buildHeap()
+     * 每次把对顶元素（max）与最后一个元素交换
+     * 然后将剩余元素堆化
+     * 直到只剩最后一个元素
+     *
+     * @param arr  待排数组
+     * @param size 数组大小
+     */
+    public static void sort(int[] arr, int size) {
+        buildHeap(arr, size);
+        int i = size;
+        while (i > 1) {
+            swap(arr, 1, size);
+            --i;
+            heapify(arr, size, i);
+        }
+    }
+
+    private static void swap(int[] arr, int i1, int i2) {
         arr[i1] = arr[i1] ^ arr[i2];
         arr[i2] = arr[i1] ^ arr[i2];
         arr[i1] = arr[i1] ^ arr[i2];
@@ -63,17 +92,19 @@ public class Heap {
 
     public static void main(String[] args) {
 
-        Heap heap = new Heap(8);
-        heap.insert(1);
-        heap.insert(3);
-        heap.insert(5);
-        heap.insert(4);
-        heap.insert(7);
-        heap.insert(9);
+//        Heap heap = new Heap(8);
+//        heap.insert(1);
+//        heap.insert(3);
+//        heap.insert(5);
+//        heap.insert(4);
+//        heap.insert(7);
+//        heap.insert(9);
 //        heap.insert(11);
-        heap.insert(7);
-        heap.remove();
-        System.out.println(heap.toString());
+//        heap.insert(7);
+//        heap.remove();
+        int[] ints = {0, 1, 12, 33, 55, 6, 5, 7};
+        Heap.sort(ints, 7);
+        System.out.println(Arrays.toString(ints));
 
     }
 
